f988bbee2c82bff56e7be6240dc07c74471a6e03
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-12 10:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function findLevel() {
+
+    let parent = document.querySelector("#level").parentElement;
+    let count = 1;
+
+    while(parent != null) {
+        console.log(parent);
+        parent = parent.parentElement;
+
+        count++;
+    }
+    alert(`The level of the element is: ${count}`);
+}
+findLevel();
